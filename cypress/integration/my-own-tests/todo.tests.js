6df20215299cd95ae77e2dd6fbf4f3649d9c8bda
eramos-devacad <eramos.devacad@gmail.com>
2021-03-23 10:31:54 +0800

    Add timeout for slower internet tests

--- cypress/integration/my-own-tests/todo.tests.js
+++ cypress/integration/my-own-tests/todo.tests.js
@@ -20,7 +20,7 @@ describe('Basic Test', () => {
     cy.contains(`todo-${random}`).should('not.exist')
     cy.get('[data-testid=todoInput]').type(`todo-${random}`)
     cy.get('[data-testid=btnSubmit]').click()
-    cy.contains(`todo-${random}`).should('exist')
+    cy.contains(`todo-${random}`, {timeout: 3 * 1000}).should('exist')
   })
 
   it('Should display confirm delete', () => {
@@ -39,7 +39,7 @@ describe('Basic Test', () => {
     cy.get('[data-testid=btnDelete-0').click()
     cy.contains('Are you sure you want to delete?').should('exist')
     cy.get('[data-testid=deleteTodo]').click()
-    cy.contains(`todo-${random}`).should('not.exist')
+    cy.contains(`todo-${random}`, {timeout: 3 * 1000}).should('not.exist')
   })
 })
 
